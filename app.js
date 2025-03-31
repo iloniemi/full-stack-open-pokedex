b9c4ed6617d5655afce95ba477e0dfaa48cfb0ae
iloniemi <juho.iloniemi@gmail.com>
2025-03-31 14:32:23 +0300

    testing webhook

--- app.js
+++ app.js
@@ -5,13 +5,11 @@ const app = express()
 const PORT = process.env.PORT || 5000
 
 app.get('/health', (req, res) => {
-  // eslint-disable-next-line no-constant-condition
-  if (true) throw new Error()
   res.send('ok')
 })
 
 app.get('/version', (req, res) => {
-  res.send('6') // change this string to ensure a new version deployed
+  res.send('7') // change this string to ensure a new version deployed
 })
 
 app.use(express.static('dist'))
